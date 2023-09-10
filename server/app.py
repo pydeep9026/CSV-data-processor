953d1d48e2d0d4dc2c109be340a88cc5bd4a1d87
vaibhavdwivedi25 <103676621+vaibhavdwivedi25@users.noreply.github.com>
2023-09-10 11:14:24 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,1 +1,99 @@
+from flask import Flask, request, make_response
+import numpy as np
+import pandas as pd
+from flask_cors import CORS
+from sklearn.linear_model import LinearRegression
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/process-csv', methods=['POST'])
+def process_csv():
+    # Retrieve the uploaded file from the request
+    file = request.files['file']
+
+    # Save the uploaded file to disk
+    file_path = 'uploaded_file.csv'
+    file.save(file_path)
+ 
+    # Load the data
+    data = pd.read_csv(file_path)
+
+    # Convert the index to a DatetimeIndex if Date column exists
+    if "Date" in data.columns:
+        data.index = pd.to_datetime(data["Date"])
+        data = data.drop("Date", axis=1)
+    elif not isinstance(data.index, pd.DatetimeIndex):
+        data.index = pd.to_datetime(data.index)
+
+    # Create a list of years
+    years = list(range(2016, 2023)) 
+
+    # Create a DataFrame to store the predictions
+    predictions = pd.DataFrame(columns=data.columns)
+
+    # For each year, predict the values
+    for year in years:
+        train_data = data[data.index.year <= year - 1]
+        test_data = data[data.index.year == year]
+
+        # Check if test_data is empty
+        if test_data.empty:
+            continue 
+
+        # Take the mod of the negative values
+        test_data[test_data < 0] = np.mod(test_data[test_data < 0], 2)
+
+        model = LinearRegression()
+        model.fit(train_data, train_data)
+
+        # Predict the values and create a DataFrame with matching column names
+        predicted_values = pd.DataFrame(model.predict(test_data), columns=data.columns, index=test_data.index)
+
+        predictions = pd.concat([predictions, predicted_values])
+
+    # Create a new row with the values for the year 2022
+    new_row = pd.DataFrame(
+        np.zeros((1, len(data.columns))),
+        columns=data.columns,
+        index=pd.to_datetime(["2022"])
+    )
+    new_row[new_row < 0] = np.mod(new_row[new_row < 0], 2)
+    data = pd.concat([data, new_row])
+
+    # Predict the 2022 data
+    train_data = data[data.index.year <= 2021]
+    test_data = data[data.index.year == 2022]
+
+    # Check if test_data is empty
+    if not test_data.empty:
+        model = LinearRegression()
+        model.fit(train_data, train_data)
+
+        # Predict the values and create a DataFrame with matching column names
+        predicted_values = pd.DataFrame(model.predict(test_data), columns=data.columns, index=test_data.index)
+
+        predictions = pd.concat([predictions, predicted_values])
+
+    # Combine the previous data and predictions
+    combined_data = pd.concat([data, predictions], axis=0)
+ 
+    # Save the combined data to an Excel file
+    output_path = 'processed_data.xlsx'
+    combined_data.iloc[:-2]._append(combined_data.iloc[[-1]]).to_excel(output_path, index=True, index_label='Date')
+
+    with open(output_path, 'rb') as file:
+        file_data = file.read()
+
+    # Create a response with the file data
+    response = make_response(file_data)
+
+    # Set the response headers for file download
+    response.headers['Content-Disposition'] = 'attachment; filename=processed_data.xlsx'
+    response.headers['Content-Type'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+    return response
+
+ 
+if _name_ == '__main__':
+    app.run()
